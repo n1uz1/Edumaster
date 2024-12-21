94b53bb8deb946ca4966f6efe54a546b781610b8
绊之绊 <138186288+bzb-bzb@users.noreply.github.com>
2024-12-21 12:36:07 +0800

    Add files via upload

---
+++ Edumaster/src/main/java/com/zcy/edumaster/EdumasterApplication.java
@@ -0,0 +1,13 @@
+package com.zcy.edumaster;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EdumasterApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EdumasterApplication.class, args);
+    }
+
+}
---
+++ Edumaster/src/main/java/com/zcy/edumaster/entity/User.java
@@ -0,0 +1,4 @@
+package com.zcy.edumaster.entity;
+
+public class User {
+}
